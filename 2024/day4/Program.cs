c2664b05dd0f5ffaa84099dfdf6a359f6f5e04fe
Matthew Esposito <mesposito@primehealthservices.com>
2024-12-05 14:59:46 -0600

    Day 5

---
+++ 2024/day4/Program.cs
@@ -0,0 +1,98 @@
+﻿using day4;
+
+string testInput = "MMMSXXMASM\r\nMSAMXMSMSA\r\nAMXSXMAAMM\r\nMSAMASMSMX\r\nXMASAMXAMM\r\nXXAMMXXAMA\r\nSMSMSASXSS\r\nSAXAMASAAA\r\nMAMMMXMMMM\r\nMXMXAXMASX";
+
+List<string> grid = new List<string>();
+
+const string XMAS = "XMAS";
+const string SAMX = "SAMX";
+
+int HorizontalCount(List<string> grid, string substring) {
+    int count = 0;
+    foreach (var row in grid) { 
+        int index = row.IndexOf(substring);
+
+        if (index != -1) {
+            count++;
+            index = row.IndexOf(substring, index + 1);
+        }
+    }
+
+    return count;
+}
+
+int VerticalCount(List<string> grid, string substring) {
+    return HorizontalCount(grid, substring);
+}
+
+int DiagonalCount(List<string> grid, string substring) {
+    int count = 0;
+    int rows = grid.Count;
+    int cols = grid[0].Length;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (i+3 >= rows || j+3 >= cols) {
+                continue;
+            }
+
+            string diagonal = grid[i][j].ToString() + grid[i+1][j+1].ToString() + grid[i + 2][j+2].ToString() + grid[i + 3][j+3].ToString();
+            if (diagonal == substring)
+            {
+                count++;
+            }
+        }
+    }
+
+    for (int k = 0; k < rows; k++) {
+        for (int l = cols-1; l >= 0; l--) {
+            if (k+3 >= rows || l-3 < 0) {
+                continue;
+            }
+
+            string diagonal = grid[k][l].ToString() + grid[k + 1][l - 1].ToString() + grid[k + 2][l - 2].ToString() + grid[k + 3][l - 3].ToString();
+            if (diagonal == substring) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+List<string> TransposeList(List<string> grid) {
+    int maxLength = grid.Max(s => s.Length);
+
+    List<string> newGrid = new List<string>();
+
+    for (int i = 0; i < maxLength; i++) {
+        string charactersAtIndex = new string(grid
+            .Where(s => s.Length > i) 
+            .Select(s => s[i])
+            .ToArray());
+
+        newGrid.Add(charactersAtIndex);
+    }
+
+    return newGrid;
+}
+
+void Part1(string input) {
+    grid = [.. input.Split("\r\n")];
+    List<string> transposedGrid = TransposeList(grid);
+
+    int total = 0;
+
+    total += HorizontalCount(grid, XMAS);
+    total += HorizontalCount(grid, SAMX);
+
+    total += VerticalCount(transposedGrid, XMAS);
+    total += VerticalCount(transposedGrid, SAMX);  
+
+    total += DiagonalCount(grid, XMAS);
+    total += DiagonalCount(grid, SAMX);
+
+    Console.WriteLine(total);
+}
+
+
+//Part1(Data.Input);
---
+++ 2024/day5/Program.cs
@@ -0,0 +1,135 @@
+﻿using day5;
+
+string rules = "47|53\r\n97|13\r\n97|61\r\n97|47\r\n75|29\r\n61|13\r\n75|53\r\n29|13\r\n97|29\r\n53|29\r\n61|53\r\n97|53\r\n61|29\r\n47|13\r\n75|47\r\n97|75\r\n47|61\r\n75|61\r\n47|29\r\n75|13\r\n53|13";
+
+string pages = "75,47,61,53,29\r\n97,61,53,29,13\r\n75,29,13\r\n75,97,47,61,53\r\n61,13,29\r\n97,13,75,29,47";
+
+Dictionary<string, List<string>> RuleDictionary(string rules) {
+    Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+    var ruleList = rules.Split("\r\n");
+
+    foreach (var rule in ruleList) { 
+        var numbers = rule.Split('|');
+        if (dictionary.ContainsKey(numbers[0])) {
+            dictionary[numbers[0]].Add(numbers[1]);
+        } else {
+            dictionary[numbers[0]] = new List<string> { numbers[1] };
+        }
+    }
+
+    return dictionary;
+}
+
+var dictionary = RuleDictionary(Data.rules);
+//foreach (var kvp in dictionary) {
+//    Console.WriteLine($"Key: {kvp.Key}, Values: {string.Join(", ", kvp.Value)}");
+//}
+
+List<string> GetValidPages(string pages) {
+    List<string> validPages = new List<string>();
+    var pagesList = pages.Split("\r\n");
+
+    foreach (var page in pagesList) { 
+        if (ValidPage(page)) {
+            validPages.Add(page);
+        }
+    }
+
+    return validPages;
+}
+
+bool ValidPage(string page) { 
+    var pageList = page.Split(",");
+
+    for (int i = 0; i < pageList.Length; i++) {
+        var validationList = dictionary.TryGetValue(pageList[i], out var values) ? values : new List<string>();
+
+        foreach (var item in validationList) {
+            if (!pageList.Contains(item)) { 
+                continue;
+            }
+
+            int validationIndex = Array.IndexOf(pageList, item);
+            if (validationIndex < i)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int GetMiddle(string page) {
+    string[] list = page.Split(',');
+    int middleIndex = list.Length / 2;
+    return int.Parse(list[middleIndex]);
+}
+
+int Total(List<string> pages) {
+    int total = 0;
+    foreach (var page in pages) {
+        total += GetMiddle(page);
+    }
+
+    return total;
+}
+
+// Part 1
+//var validPages = GetValidPages(Data.pages);
+//Console.WriteLine(Total(validPages));
+
+
+// Part 2
+
+List<string> GetInValidPages(string pages) {
+    List<string> validPages = new List<string>();
+    var pagesList = pages.Split("\r\n");
+
+    foreach (var page in pagesList) {
+        if (!ValidPage(page)) {
+            validPages.Add(page);
+        }
+    }
+
+    return validPages;
+}
+
+string FixPage(string page) {
+    var pageList = page.Split(",");
+
+    for (int i = 0; i < pageList.Length; i++) {
+        var validationList = dictionary.TryGetValue(pageList[i], out var values) ? values : new List<string>();
+
+        foreach (var item in validationList) {
+            if (!pageList.Contains(item)) {
+                continue;
+            }
+
+            int validationIndex = Array.IndexOf(pageList, item);
+            if (validationIndex < i) {
+                pageList[validationIndex] = pageList[i];
+                pageList[i] = item;
+            }
+        }
+    }
+
+    return string.Join(",", pageList);
+}
+
+string FixedPage(string page) {
+    var newPage = FixPage(page);
+    if (ValidPage(newPage)) {
+        return newPage;
+    }
+
+    return FixedPage(newPage);
+}
+
+var invalid = GetInValidPages(Data.pages);
+List<string> newPages = new List<string>();
+foreach(var page in invalid) {
+    var newPage = FixedPage(page);
+    newPages.Add(newPage);
+}
+Console.WriteLine(Total(newPages));
