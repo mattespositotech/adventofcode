e32f28f97192a2d4c468dbe35c0663da4b0aea31
Matthew Esposito <mesposito@primehealthservices.com>
2024-12-07 12:19:02 -0600

    Day 6 and 7

---
+++ 2024/day6/Program.cs
@@ -0,0 +1,259 @@
+﻿using day6;
+using System.Drawing;
+using System.Reflection;
+
+string testInput = "....#.....\r\n.........#\r\n..........\r\n..#.......\r\n.......#..\r\n..........\r\n.#..^.....\r\n........#.\r\n#.........\r\n......#...";
+
+const char OBSTRUCTION = '#';
+const char NEW_OBSTRUCTION = '%';
+const char MARK = 'X';
+const char MARK2 = '0';
+const char MARK3 = '3';
+const string GAURD = "^v><";
+
+char[][] MakeMap(string input) {
+    string[] rows = input.Split("\r\n");
+
+    char[][] map = new char[rows.Length][];
+    for (int i = 0; i < rows.Length; i++) {
+        map[i] = rows[i].ToCharArray();
+    }
+
+    return map;
+}
+
+void PrintMap(char[][] map) {
+    foreach (var row in map) {
+        Console.WriteLine(string.Join("", row));
+    }
+    Console.WriteLine("----------------------------------");
+}
+
+Tuple<int, int> FindGuard(char[][] map) {
+    for (int i = 0; i < map.Length; i++) {
+        for (int j = 0; j < map[i].Length; j++) {
+            if (GAURD.Contains(map[i][j])) {
+                return new Tuple<int, int>(i, j);
+            }
+        }
+    }
+
+    throw new Exception("Bad Input");
+}
+
+void TraverseMap(char[][] map) {
+    int x = 0;
+    int y = -1;
+    int index = 0;
+
+    void Turn90Deg() {
+        (int, int)[] directions = { (0, -1), (1, 0), (0, 1), (-1, 0) };
+
+        index++;
+        if (index >= directions.Length) {
+            index = 0;
+        }
+
+        (x, y) = directions[index];
+    }
+
+    Tuple<int, int> guardStart = FindGuard(map);
+
+
+    int bounds = map.Length;
+    //Console.WriteLine($"Bounds: {bounds}");
+
+    int guardX = guardStart.Item2;
+    int guardY = guardStart.Item1;
+    //Console.WriteLine($"Start Index: {guardX}, {guardY}");
+
+    while ((bounds > guardX + x && guardX + x >= 0)
+        && (bounds > guardY + y && guardY + y >= 0)) {
+        //Console.WriteLine($"Cords to check: {guardX + x}, {guardY + y}, current: {guardX}, {guardY}");
+        if (map[guardY + y][guardX + x] == OBSTRUCTION) {
+            Turn90Deg();
+        }
+        map[guardY][guardX] = MARK;
+        guardX += x;
+        guardY += y;
+        //PrintMap(map);
+    }
+
+    map[guardY][guardX] = MARK;
+}
+
+int CountX(char[][] map) {
+    int count = 0;
+
+    for (int i = 0; i < map.Length; i++) {
+        for (int j = 0; j < map[i].Length; j++) {
+            if (map[i][j] == MARK) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+// Part 1
+var map = MakeMap(Data.Input);
+//TraverseMap(map);
+//Console.WriteLine(CountX(map)); 
+
+// Part 2
+
+// Get current path and then loop that list as possible places for new obstical
+// for each add to a dictionary - at starting position check if we have looped threw all coords twice if so return new obstical indexes
+// else move to new check
+
+List<(int, int)> GetInitalPath(char[][] map, Tuple<int, int> guardStart) {
+    List<(int, int)> path = new List<(int, int)> ();
+    int x = 0;
+    int y = -1;
+    int index = 0;
+
+    void Turn90Deg() {
+        (int, int)[] directions = { (0, -1), (1, 0), (0, 1), (-1, 0) };
+
+        index++;
+        if (index >= directions.Length) {
+            index = 0;
+        }
+
+        (x, y) = directions[index];
+    }
+
+    int bounds = map.Length;
+
+    int guardX = guardStart.Item2;
+    int guardY = guardStart.Item1;
+
+    while ((bounds > guardX + x && guardX + x >= 0)
+        && (bounds > guardY + y && guardY + y >= 0)) {
+        if (map[guardY + y][guardX + x] == OBSTRUCTION) {
+            Turn90Deg();
+        }
+        path.Add((guardY, guardX));
+        guardX += x;
+        guardY += y;
+    }
+
+    if (! path.Contains((guardY, guardX))){
+        path.Add((guardY, guardX));
+    }
+
+    // remove guard start
+    path.RemoveAt(0);
+
+    //Console.WriteLine($"Path Length: {path.Count}");
+    List<(int, int)> uniquePoints = new List<(int, int)>(new HashSet<(int, int)>(path));
+    //Console.WriteLine($"Path Length: {uniquePoints.Count}");
+
+    return uniquePoints;
+}
+
+bool CheckCircular(char[][] map, Tuple<int, int> guardStart) {
+    int x = 0;
+    int y = -1;
+    int index = 0;
+    int loopCount = 0;
+
+    Dictionary<(int, int), int> circlePath = new Dictionary<(int, int), int>();
+
+    void Turn90Deg() {
+        (int, int)[] directions = { (0, -1), (1, 0), (0, 1), (-1, 0) };
+
+        index++;
+        if (index >= directions.Length) {
+            index = 0;
+        }
+
+        (x, y) = directions[index];
+    }
+
+    //PrintMap(map);
+
+    int bounds = map.Length;
+
+    int guardX = guardStart.Item2;
+    int guardY = guardStart.Item1;
+    //Console.WriteLine($"Start Index: {guardX}, {guardY}");
+
+    while ((bounds > guardX + x && guardX + x >= 0)
+        && (bounds > guardY + y && guardY + y >= 0)) {
+        //Console.WriteLine($"Cords to check: {guardX + x}, {guardY + y}, current: {guardX}, {guardY}");
+        if (map[guardY + y][guardX + x] == OBSTRUCTION || map[guardY + y][guardX + x] == NEW_OBSTRUCTION) {
+            Turn90Deg();
+        }
+
+        if (map[guardY][guardX] == MARK) {
+            map[guardY][guardX] = MARK2;
+        } else if (map[guardY][guardX] == MARK2) {
+            map[guardY][guardX] = MARK3;
+        }
+        else if (map[guardY][guardX] == MARK3) {
+            if(circlePath.ContainsKey((guardY, guardX))) {
+                circlePath[(guardY, guardX)]++;
+                if (circlePath.Values.All(count => count >= 2)) {
+                    return true;
+                }
+            } else {
+                circlePath[(guardY, guardX)] = 1;
+            }
+        }else {
+            map[guardY][guardX] = MARK;
+        }
+
+
+        guardX += x;
+        guardY += y;
+        //PrintMap(map);
+
+        //loopCount++;
+        //if (loopCount > 1000) {
+        //    PrintMap(map);
+        //    var list = circlePath.Where(kvp => kvp.Value < 2).ToList();
+        //    Console.WriteLine(string.Join(", ", list));
+        //    loopCount = 0;
+        //}
+
+    }
+    return false;
+}
+
+
+
+char[][] CreateNewMap(char[][] map, (int, int) newObstruction) {
+    char[][] newMap = new char[map.Length][];
+    for (int i = 0; i < map.Length; i++) {
+        newMap[i] = new char[map[i].Length];
+        Array.Copy(map[i], newMap[i], newMap.Length);
+    }
+
+    newMap[newObstruction.Item1][newObstruction.Item2] = NEW_OBSTRUCTION;
+    return newMap;
+}
+
+
+//map = MakeMap(testInput);
+Tuple<int, int> guardStart = FindGuard(map);
+var path = GetInitalPath(map, guardStart);
+
+int count = 0;
+
+//var newMap = CreateNewMap(map, (105, 36));
+//Console.WriteLine(CheckCircular(newMap, guardStart));
+//Console.WriteLine();
+
+foreach (var obstructionCoords in path) {
+    Console.WriteLine(obstructionCoords);
+    var newMap = CreateNewMap(map, obstructionCoords);
+    if (CheckCircular(newMap, guardStart)) {
+        count++;
+        //Console.WriteLine($"Total: {count}");
+        //PrintMap(newMap);
+    }
+}
+
+Console.WriteLine($"Total: {count}");
---
+++ 2024/day7/Program.cs
@@ -0,0 +1,64 @@
+﻿using day7;
+
+string testInput = "190: 10 19\r\n3267: 81 40 27\r\n83: 17 5\r\n156: 15 6\r\n7290: 6 8 6 15\r\n161011: 16 10 13\r\n192: 17 8 14\r\n21037: 9 7 18 13\r\n292: 11 6 16 20";
+
+List<char[]> GenerateCombinations(int length, char[] operators) {
+    List<char[]> results = new List<char[]>();
+    GenerateRecursive(new char[length], 0, length, operators, results);
+    return results;
+}
+
+void GenerateRecursive(char[] current, int position, int length, char[] operators, List<char[]> results) {
+    if (position == length) {
+        results.Add((char[])current.Clone());
+        return;
+    }
+
+    foreach (char op in operators) {
+        current[position] = op;
+        GenerateRecursive(current, position + 1, length, operators, results);
+    }
+}
+
+long ValidEquation(string equation) {
+    var numbers = equation.Split(' ').ToList();
+
+    long num2check = long.Parse(numbers[0][..^1]);
+
+    numbers.RemoveAt(0);
+
+    List<char[]> combinations = GenerateCombinations(numbers.Count - 1, ['+', '*', '|']);
+
+    foreach (var combo in combinations) {
+        long total = long.Parse(numbers[0]);
+        for (int i = 0; i < combo.Length; i++) {
+            if (combo[i] == '+') {
+                total += long.Parse(numbers[i + 1]);
+            } else if (combo[i] == '*') {
+                total *= long.Parse(numbers[i + 1]);
+            } else if (combo[i] == '|') {
+                string conncat = total.ToString() + numbers[i + 1];
+                total = long.Parse(conncat);
+            }
+        }
+
+        if (total == num2check) {
+            return num2check;
+        }
+    }
+
+    return 0;
+}
+
+long Part1() {
+    long total = 0;
+    var equations = Data.Input.Split("\r\n");
+    //var equations = testInput.Split("\r\n");
+    foreach (var equation in equations) {
+        total += ValidEquation(equation);
+    }
+
+    return total;
+}
+
+Console.WriteLine(Part1());
